6327e2db43ae39daa7940ae09ab5e05360b75ef9
Gina Sprint <sprint@gonzaga.edu>
2019-09-12 14:22:24 -0700

    initial commit

---
+++ src/PublicationTester.java
@@ -0,0 +1,16 @@
+public class PublicationTester {
+    public static void main(String[] args) {
+        // inheritance: a relationship between two classes
+        // where one class includes ("inherits") the state
+        // AND behavior of the other class
+
+        // Publication is going to be a super class
+        // task: create a Publication class
+        // have title, publisher, number of pages, price
+        // toString(): return comma separated list fields values
+        // DVC and EVC
+        // no need for getters and setters
+
+        // make a few Publication objects
+    }
+}
